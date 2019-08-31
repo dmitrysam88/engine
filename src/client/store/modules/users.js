b6febad91754867086dbc0b8004257cc78ac8120
Dmitry Sam <dmitry.sam88@gmail.com>
2019-08-31 09:56:09 +0300

    server request has been added

--- src/client/store/modules/users.js
+++ src/client/store/modules/users.js
@@ -1,20 +1,23 @@
 import { createAction, handleActions } from 'redux-actions';
 import { call, put, takeEvery, takeLatest, actionChannel } from 'redux-saga/effects';
+import axios from 'axios';
 
 // Actions
 
 // let addUser = createAction('ADD_NEW_USER', user => user);
 
 const actions = { 
-    addUser: createAction('ADD_NEW_USER', user => user),
+    addUser: createAction('ADD_NEW_USER', (user) => { return { user }}),
     getUsers: createAction('FETCH_USERS') 
 };
 
 function* sagaFetchUsers(action){
     yield put({ type: 'FETCH_USERS_STARTED' });
     try {
-        yield put({ type: 'FETCH_USERS_SUCCEEDED' });
+        let result = yield axios.get('http://localhost:3000/api/users');
+        yield put({ type: 'FETCH_USERS_SUCCEEDED', payload: { users: result.data }});
     } catch (error) {
+        console.log(error);
         yield put({ type: 'FETCH_USERS_ERROR' });
     }
 }
@@ -29,12 +32,11 @@ function* rootSaga(){
 const reducers = handleActions({
     'ADD_NEW_USER': (state, action) => {
         let users = state.users.slice();
-        users.push(action.payload);
+        users.push(action.payload.user);
         return Object.assign({}, state, { users: users });
     },
-    'FETCH_USERS_SUCCEEDED': (state, action) => {
-        console.log('FETCH_USERS_SUCCEEDED');
-        return state;
+    'FETCH_USERS_SUCCEEDED': (state, action) => {        
+        return Object.assign({}, state, { users: action.payload.users });
     }
 }, { users: [] });
 
